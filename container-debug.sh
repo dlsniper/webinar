4ecc6799412c09b8345b8e6c703bdcc09f25c119
Florin Patan <florinpatan@gmail.com>
2017-06-12 16:44:13 +0100

    Make script executable

--- container-debug.sh
+++ container-debug.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t webinar:debug .
-docker run --rm --name=webinar-debug -p 8000:8000 -p 2345:40000 --security-opt=seccomp:unconfined webinar:debug
+docker run --rm --name=webinar-debug -p 8000:8000 -p 40000:40000 --security-opt=seccomp:unconfined webinar:debug
